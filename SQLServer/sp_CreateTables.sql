39ac26879ce468ccd87b022a5bdf1251dd0d5a04
MarioQuirosLuna <marioquiros21@yahoo.com>
2022-05-16 11:12:34 -0600

    Correccion modelo estrella

--- SQLServer/sp_CreateTables.sql
+++ SQLServer/sp_CreateTables.sql
@@ -8,7 +8,6 @@ BEGIN
 	(
 		Product_id INT PRIMARY KEY NOT NULL,
 		Name VARCHAR(30) NOT NULL,
-		Amount_Of_Sales INT NOT NULL DEFAULT(0),
 		Price NUMERIC(6,2) NOT NULL
 	)
 
@@ -16,8 +15,15 @@ BEGIN
 	(
 		Sales_Head_id INT PRIMARY KEY NOT NULL,
 		Paid_In_Cash BIT NOT NULL,
-		Paid_Type_Description VARCHAR(50) NOT NULL,
-		Date_Of_Sale DATE NOT NULL
+		Paid_Type_Description VARCHAR(50) NOT NULL
+	)
+
+	CREATE TABLE SALES.DIM_DATES
+	(
+		Sales_Head_Date_id INT PRIMARY KEY NOT NULL,
+		Date_Of_Sale DATE NOT NULL,
+		Date_Year VARCHAR(50) NOT NULL,
+		Date_Month VARCHAR(50) NOT NULL
 	)
 
 	CREATE TABLE SALES.FACT_SALES
@@ -25,10 +31,13 @@ BEGIN
 		Id INT PRIMARY KEY NOT NULL,
 		Product_id INT NOT NULL,
 		Sales_Head_id INT NOT NULL,
+		Sales_Head_Date_id INT NOT NULL,
+		Amount_Of_Sales INT NOT NULL DEFAULT(0),
 		SubTotal_Price NUMERIC(9,2) NOT NULL,
 		Total_Price NUMERIC(9,2) NOT NULL,
 		FOREIGN KEY (Product_id) REFERENCES SALES.DIM_Products(Product_id),
-		FOREIGN KEY (Sales_Head_id) REFERENCES SALES.DIM_Sales_Head(Sales_Head_id)
+		FOREIGN KEY (Sales_Head_id) REFERENCES SALES.DIM_Sales_Head(Sales_Head_id),
+		FOREIGN KEY (Sales_Head_Date_id) REFERENCES SALES.DIM_DATES(Sales_Head_Date_id)
 	)
 END
 GO
